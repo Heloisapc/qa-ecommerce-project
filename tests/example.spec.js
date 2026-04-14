707bfccd453862df84ad8b53b92b293655d8f4dd
Heloisapc <heloisapcorreia@gmail.com>
2026-04-14 14:12:36 -0300

    Adicionando teste de checkout com validação de erro

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -47,3 +47,21 @@ await page.click('#remove-sauce-labs-backpack');
 
 await expect(page.locator('.shopping_cart_badge')).toHaveCount(0);
 });
+
+test('checkout com primeiro nome vazio', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  await page.click('#add-to-cart-sauce-labs-backpack');
+  await page.click('.shopping_cart_link');
+  await page.click('#checkout');
+
+  await page.fill('#last-name', 'Silva');
+  await page.fill('#postal-code', '12345');
+  await page.click('#continue');
+
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
